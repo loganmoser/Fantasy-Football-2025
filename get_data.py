39da02f8221d899630e29ae4c8335bc7534cf1c6
Logan Moser <loganmoser10@protonmail.com>
2025-05-04 12:49:40 -0500

    Clean player name data and create columns for pro-bowl and all-pro distinctions

--- get_data.py
+++ get_data.py
@@ -21,6 +21,19 @@ def get_season(season):
         print(f"Fetching data for {season} season...")
         curr_season = pd.read_html(io=url, header=1)
         season_df = curr_season[0]
+        
+        #Data cleaning to clean up player names and drop headers
+        # Every 31 lines the headers of the file reappear so we need to drop these rows where Rk = Rk
+        season_df = season_df.drop(season_df[season_df['Rk'] == 'Rk'].index)
+
+        players = season_df['Player']
+        pro_bowl = [1 if "*" in player else 0  for player in players]
+        all_pro = [1 if "+" in player else 0 for player in players]
+        season_df['Pro_bowl'] = pro_bowl
+        season_df['All_pro'] = all_pro
+        season_df['Player'] = season_df['Player'].str.strip('\*+')
+        
+
         # Saving 2025 season for future use
         file_path = os.path.abspath(os.path.join(os.getcwd(), f"data/seasons/{season}_fantasy.csv"))
         print(f"Attempting to save to: {file_path}")
@@ -39,10 +52,14 @@ def get_player(player):
 #If the 2025 fantasy rankings don't already exist - get them
 for i in range(2024, 2019, -1):
     if os.path.isfile(os.path.join(os.getcwd(), f"data/seasons/{i}_fantasy.csv")):
-        print(f"Data for the {i} season already exists...")
-        continue
+        user_input = input(f"Data for the {i} season already exists do you want to replace it? Y/n: ")
+        if (user_input == 'n'):
+            continue
+        elif (user_input == '' or user_input.toUpper() == 'Y'):
+            get_season(i)
     else:
         get_season(i)
 
 #Getting player info from the most recent season, going through the top 100 and getting career stats 
+ff24_df = pd.read_csv('data/seasons/2024_fantasy.csv')
 
