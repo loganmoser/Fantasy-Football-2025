3530f0dce9945ca55937d64c3426d4cde6ea798d
Logan Moser <loganmoser10@protonmail.com>
2025-05-04 08:10:58 -0500

    Create get_data and get_player functions

---
+++ get_data.py
@@ -0,0 +1,48 @@
+import requests
+import pandas as pd
+import os
+from pathlib import Path
+
+# If seasons or player subdirectory does not exist create it
+def check_dir(subdir):
+    if not os.path.isdir(os.path.join(os.getcwd(), f"data/{subdir}/")):
+        print(f"Directory 'data/{subdir}' does not exist")
+        print(f"Creating 'data/{subdir}'...")
+        os.mkdir(os.path.abspath(os.path.join(os.getcwd(), f"data/{subdir}/")))
+
+
+def get_season(season):
+    try:
+        # Check for the directory and if it does not exist create it
+        check_dir('seasons')
+        # By passing in unique seasons we can quickly get the url
+        url = f"https://www.pro-football-reference.com/years/{season}/fantasy.htm" 
+        # Pandas implementation using read_html we can get the 2025 season and skip the first row for headers
+        print(f"Fetching data for {season} season...")
+        curr_season = pd.read_html(io=url, header=1)
+        season_df = curr_season[0]
+        # Saving 2025 season for future use
+        file_path = os.path.abspath(os.path.join(os.getcwd(), f"data/seasons/{season}_fantasy.csv"))
+        print(f"Attempting to save to: {file_path}")
+        season_df.to_csv(file_path)
+    except Exception as e:
+        print(f"Error processing {season} season: {str(e)}")
+        return False
+
+def get_player(player):
+    try:
+        check_dir('players')
+    except Exception as e:
+        print(f"Error processing {player}: {str(e)}")
+        return False
+
+#If the 2025 fantasy rankings don't already exist - get them
+for i in range(2024, 2019, -1):
+    if os.path.isfile(os.path.join(os.getcwd(), f"data/seasons/{i}_fantasy.csv")):
+        print(f"Data for the {i} season already exists...")
+        continue
+    else:
+        get_season(i)
+
+#Getting player info from the most recent season, going through the top 100 and getting career stats 
+
