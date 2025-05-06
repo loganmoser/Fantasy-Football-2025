dbb440808504df71675dd1691972809d6cda2087
Logan Moser <loganmoser10@protonmail.com>
2025-05-05 21:45:54 -0500

    Create funciton to get players career data and invidual season game logs

--- get_data.py
+++ get_data.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 import time
 
+counter = 0
+
 # If seasons or player subdirectory does not exist create it
 def check_dir(subdir):
     dir_path = Path(f"data/{subdir}/")
@@ -15,6 +17,7 @@ def check_dir(subdir):
 
 def get_season(season):
     try:
+        global counter
         # Check for the directory and if it does not exist create it
         check_dir('seasons')
         # By passing in unique seasons we can quickly get the url
@@ -22,6 +25,12 @@ def get_season(season):
         # Pandas implementation using read_html we can get the 2025 season and skip the first row for headers
         print(f"Fetching data for {season} season...")
         curr_season = pd.read_html(io=url, header=1)
+        # After making a request we need to make sure we are within site rules
+        counter += 1
+        if counter % 10 == 0:
+            print("10 requests reached. Stopping for 60 seconds...")
+            time.sleep(60)
+
         season_df = curr_season[0]
         season_df.set_index('Rk', drop=False)
         #Data cleaning to clean up player names and drop headers
@@ -35,7 +44,6 @@ def get_season(season):
         season_df['All_pro'] = all_pro
         season_df['Player'] = season_df['Player'].str.strip('*+')
         
-
         # Saving 2025 season for future use
         file_path = os.path.abspath(os.path.join(os.getcwd(), f"data/seasons/{season}_fantasy.csv"))
         print(f"Attempting to save to: {file_path}")
@@ -46,37 +54,47 @@ def get_season(season):
 
 def get_player(player):
     try:
+        global counter
         # To track requests and comply with rate limits
-        counter = 0
         # Check for players directory if not present -- create it
         check_dir(f'players/{player}')
         # Each player has a unique url for their stats page made up of /first letter of last name /first four of last name first two of first name00
         first_name = player.split(' ')[0]
         last_name = player.split(' ')[1]
-        player_base_url = f"https://www.pro-football-reference.com/players/{last_name[0].toUpper()}/{last_name[:4]}{first_name[:2]}00"
+        player_base_url = f"https://www.pro-football-reference.com/players/{last_name[0].upper()}/{last_name[:4]}{first_name[:2]}00"
         player_url = f"{player_base_url}.htm"
-
         # Create career data frame for analysis
-        career_df = pd.read_html(io=player_url)
-        # we need to stay under 10 requests a minute
+        career_list = pd.read_html(io=player_url, header=1)
+        career_df = career_list[0]
+        career_df.set_index('Season', drop=False)
+        career_df.to_csv(f'data/players/{player}/{player}_career.csv')
+
+        # We need to stay under 10 requests a minute
         counter += 1
-        if counter == 10:
+        if counter % 10 == 0:
+            print("10 requests reached. Stopping for 60 seconds...")
             time.sleep(60)
 
         # Get individual seasons for time-based trend analysis
-        seasons = career_df['Season']
-
+        seasons = [int(season) for season in career_df['Season'] if str(season).strip().isdigit()]
         for season in seasons:
+            print(f"Fetching data for {player}'s season in {year}")
             season_url = f"{player_base_url}/gamelog/{season}"
-            season_df = pd.read_html(io=season_url, header=1)
-
+            season_df = pd.read_html(io=season_url, header=1)[0]
+            
+            counter += 1
+            if counter % 10 == 0:
+                print("10 requests reached. Stopping for 60 seconds...")
+                time.sleep(60)
+            
+            season_df.to_csv(f'data/players/{player}/{player}_{season}.csv')
     except Exception as e:
         print(f"Error processing {player}: {str(e)}")
         return False
 
 #If the 2025 fantasy rankings don't already exist - get them
 for i in range(2024, 2019, -1):
-  if os.path.isfile(os.path.join(os.getcwd(), f"data/seasons/{i}_fantasy.csv")):
+  if Path(f"data/seasons/{i}_fantasy.csv").exists():
       print(f'Data for the {i} season already exists. Continuing to the next season...')
       continue
   else:
@@ -88,4 +106,11 @@ ff24_df = pd.read_csv('data/seasons/2024_fantasy.csv')
 # We only want top 200 performs and skip unnamed 0 column
 top_performers = ff24_df.iloc[:200, 1:]
 
-top_players = top_performers['Player']
+players = top_performers['Player']
+
+for player in players:
+    if Path(f"data/players/{player}").exists():
+        print(f'Data for {player} already exists. Continuing to the next player...')
+        continue
+    else:
+        get_player(player)
